b43335fd2dd5b4fe9efb3636f57c297c2064b604
Justin Calleja <calleja.justin@gmail.com>
2018-10-08 14:25:11 +0200

    Updates

---
+++ precache-manifest.fb1c2a0154f1a47eb9f76c784a5b33c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40fc53c9f123be5691f5",
+    "url": "/carousel-project/static/js/runtime~main.40fc53c9.js"
+  },
+  {
+    "revision": "de2e65fa07ca8315b81f",
+    "url": "/carousel-project/static/js/main.de2e65fa.chunk.js"
+  },
+  {
+    "revision": "753ac34512137c4c86bc",
+    "url": "/carousel-project/static/js/1.753ac345.chunk.js"
+  },
+  {
+    "revision": "753ac34512137c4c86bc",
+    "url": "/carousel-project/static/css/1.c8fa64cc.chunk.css"
+  },
+  {
+    "revision": "4520671967f7f77764a7f613694cca78",
+    "url": "/carousel-project/index.html"
+  }
+];
